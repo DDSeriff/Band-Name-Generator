9a7b004746c5652adef3e987b30c0faaa0e32b0c
Nicholas Miller <aadenmiller@gmail.com>
2024-03-28 18:14:37 -0400

    First commit

---
+++ Band Name Generator.py
@@ -0,0 +1,5 @@
+Greeting = "Alright, let's get you a name for your band.\n"
+print(Greeting)
+City = input("Where'd you grow up? Like, which city?\n")
+Pet = input("Cool, cool. And what about the name of your favorite pet?\n")
+print("Sweet, so then using my powerful prognostication abilities, I think your band name could be " + City + " " + Pet + "!")
